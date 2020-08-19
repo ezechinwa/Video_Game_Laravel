e48ae035b729358ba53c579eb8f8e4e84732bcfe
Chinwa Williams <williams.chinwa@tizeti.com>
2020-08-19 12:43:48 +0100

    added page responsiveness

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
         <div class="container mx-auto px-4">
             <div class="h2 text-blue-500 uppercase tracking-wide font-semibold">Popular games</div>
             
-            <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16"> 
+            <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16"> 
                
                 <div class="game mt-8">
                     <div class="relative inline-block">
@@ -223,12 +223,13 @@
             </div>  {{-- end popular-games --}}
 
 
-            <div class=" flex my-10">
-                <div class="recently-reviewed w-3/4 mr-32">
+            <div class=" flex flex-col lg:flex-row my-10">
+                <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
+
                   <h2 class="text-blue-500 uppercase tracking-wide font-semibold"> Recently Reviewed</h2>
-                  <div class="recently-reviewed-container space-y-12 mt-8">
-                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6"> 
-                        
+                   
+                     <div class="recently-reviewed-container space-y-12 mt-8">
+                         <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6"> 
                             <div class="relative flex-none">
                                 <a href="#">
                                     <img src="/images/ff7.jpg" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
@@ -247,11 +248,7 @@
                                    <div class="text-gray-400 mt-1">Playstation 4</div>
                                    <p class="mt-6 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim odit fuga impedit officiis repellat repellendus, sed ex reiciendis quibusdam eos provident voluptatem error velit similique expedita? Cumque et dolorem qui!<p>
                             </div>
-                     
-
-
-
-                    </div> {{-- end game --}}
+                       </div> {{-- end game --}}
 
 
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6"> 
@@ -274,11 +271,7 @@
                                <div class="text-gray-400 mt-1">Playstation 4</div>
                                <p class="mt-6 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim odit fuga impedit officiis repellat repellendus, sed ex reiciendis quibusdam eos provident voluptatem error velit similique expedita? Cumque et dolorem qui!<p>
                         </div>
-                 
-
-
-
-                </div> {{-- end game --}}
+                   </div> {{-- end game --}}
 
 
                 <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6"> 
@@ -300,19 +293,126 @@
                            </a>
                            <div class="text-gray-400 mt-1">Playstation 4</div>
                            <p class="mt-6 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim odit fuga impedit officiis repellat repellendus, sed ex reiciendis quibusdam eos provident voluptatem error velit similique expedita? Cumque et dolorem qui!<p>
-                    </div>
-             
-
-
-
+                    </div>  
             </div> {{-- end game --}}
-                  </div>
-               
-                </div> {{-- end recently-reviewed --}}
+
+            </div>
+                
+       </div> {{-- end recently-reviewed --}}
     
-                <div class="most-anticipated  w-1/4"> 
+                <div class="most-anticipated   lg:w-1/4 mt-12 lg:mt-0"> 
+
+                    <h2 class="text-blue-500 uppercase tracking-wide font-semibold"> Most Anticipated</h2>
                
+                    <div class="most-anticipated-container space-y-10 mt-8"> 
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="/images/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Cyberpunk 77
+                                   </a>
+                                   <p class="text-gray-400 text-sm mt-1">Sept 16 2020</p>
+                            </div>
+                        </div>
+
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="/images/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Cyberpunk 77
+                                   </a>
+                                   <p class="text-gray-400 text-sm mt-1">Sept 16 2020</p>
+                            </div>
+                        </div>
+
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="/images/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Cyberpunk 77
+                                   </a>
+                                   <p class="text-gray-400 text-sm mt-1">Sept 16 2020</p>
+                            </div>
+                        </div>
+
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="/images/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">
+                                    Cyberpunk 77
+                                   </a>
+                                   <p class="text-gray-400 text-sm mt-1">Sept 16 2020</p>
+                            </div>
+                        </div>
+
+                        <h2 class="text-blue-500 uppercase tracking-wide font-semibold"> Coming soon</h2>
                
+                        <div class="most-anticipated-container space-y-10 mt-8"> 
+                            <div class="game flex">
+                                <a href="#">
+                                    <img src="/images/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                                </a>
+    
+                                <div class="ml-4">
+                                    <a href="#" class="hover:text-gray-300">
+                                        Cyberpunk 77
+                                       </a>
+                                       <p class="text-gray-400 text-sm mt-1">Sept 16 2020</p>
+                                </div>
+                            </div>
+    
+                            <div class="game flex">
+                                <a href="#">
+                                    <img src="/images/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                                </a>
+    
+                                <div class="ml-4">
+                                    <a href="#" class="hover:text-gray-300">
+                                        Cyberpunk 77
+                                       </a>
+                                       <p class="text-gray-400 text-sm mt-1">Sept 16 2020</p>
+                                </div>
+                            </div>
+    
+                            <div class="game flex">
+                                <a href="#">
+                                    <img src="/images/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                                </a>
+    
+                                <div class="ml-4">
+                                    <a href="#" class="hover:text-gray-300">
+                                        Cyberpunk 77
+                                       </a>
+                                       <p class="text-gray-400 text-sm mt-1">Sept 16 2020</p>
+                                </div>
+                            </div>
+    
+                            <div class="game flex">
+                                <a href="#">
+                                    <img src="/images/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                                </a>
+    
+                                <div class="ml-4">
+                                    <a href="#" class="hover:text-gray-300">
+                                        Cyberpunk 77
+                                       </a>
+                                       <p class="text-gray-400 text-sm mt-1">Sept 16 2020</p>
+                                </div>
+                            </div>
+
+                    </div>
                 </div> {{-- end most-anticipated  --}}
             </div>
 
